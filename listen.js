b22c21f280a266cad0b68c7585144f0e20329eae
Zsolt <zsolt@wavedropper.com>
2026-02-04 12:45:13 +0000

    Update listen.js to comply with commonJS

--- listen.js
+++ listen.js
@@ -1,10 +1,16 @@
-import app from "./app.js";
-import dotenv from "dotenv";
+const app = require("./app.js");
+const dotenv = require("dotenv");
 
 const ENV = process.env.NODE_ENV || "development";
 dotenv.config({ path: `${process.cwd()}/.env.${ENV}` });
 
 const { PORT } = process.env;
+
+if (!process.env.PGDATABASE) {
+  throw new Error("No PGDATABASE configured");
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
