13a3959ab6dbd8c80e45fca699c0d7726e2544cc
Zsolt <zsolt@wavedropper.com>
2026-02-08 21:05:44 +0000

    Add validator utility methods

--- services/utils.js
+++ services/utils.js
@@ -1,6 +1,8 @@
+const BadRequestError = require("../errors/BadRequestError");
 const InvalidTypeError = require("../errors/InvalidTypeError");
 const NotFoundError = require("../errors/NotFoundError");
 const { fetchArticleById } = require("../models/articles.model");
+const { fetchTopicBySlug } = require("../models/topics.model");
 const { fetchUserByUsername } = require("../models/users.model");
 
 exports.validateArticleId = async (article_id) => {
@@ -18,4 +20,28 @@ exports.validateIdFormat = (id, id_type = "") => {
 exports.validateUsername = async (username) => {
   if ((await fetchUserByUsername(username)) === undefined)
     throw new NotFoundError("User not found!");
+};
+
+exports.validateArticleSortByColumnName = (sort_by) => {
+  const whitelist = [
+    "article_id",
+    "title",
+    "topic",
+    "author",
+    "created_at",
+    "votes",
+  ];
+  if (!whitelist.includes(sort_by))
+    throw new BadRequestError(`Sorting by ${sort_by} isn't allowed!`);
+};
+
+exports.validateOrder = (order) => {
+  const whitelist = ["asc", "desc"];
+  if (!whitelist.includes(order))
+    throw new BadRequestError(`Order "${order}" is invalid!`);
+};
+
+exports.validateTopicSlug = async (slug) => {
+  if ((await fetchTopicBySlug(slug)) === undefined)
+    throw new NotFoundError("Topic not found!");
 };
