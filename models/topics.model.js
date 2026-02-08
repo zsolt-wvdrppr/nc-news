cc3b0b1d0cc7eb196742e21f06fb676d9534eaf3
Zsolt <zsolt@wavedropper.com>
2026-02-08 21:04:08 +0000

    Add fetchTopicBySlug method to topics model layer

--- models/topics.model.js
+++ models/topics.model.js
@@ -4,3 +4,8 @@ exports.fetchAllTopics = async () => {
   const result = await db.query("SELECT * FROM topics;");
   return result.rows;
 };
+
+exports.fetchTopicBySlug = async (slug) => {
+  const result = await db.query("SELECT * FROM topics WHERE slug = $1", [slug]);
+  return result.rows[0];
+};
