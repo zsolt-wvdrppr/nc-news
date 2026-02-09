b0cc4392c0bbf98720858f5310dfc75fc58386d2
Zsolt <zsolt@wavedropper.com>
2026-02-09 09:35:09 +0000

    Prepare for production db

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,22 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
+const config = {};
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not configured");
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const db = new Pool(config);
 
 module.exports = db;
